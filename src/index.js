fa9be8aaa7ab356746a2ff5b3ea3e57336160c4e
Thomas GERVAIS <thomas.gervais@st.com>
2019-06-11 11:15:31 +0200

    feat(package): Add clean-webpack-plugin

--- src/index.js
+++ src/index.js
@@ -1,35 +1,46 @@
 const mix = require('laravel-mix');
 const pkg = require('read-pkg-up');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-mix.extend('bundle', new class {
+mix.extend(
+	'bundle',
+	new class {
+		register(params) {
+			Config.sourceFile = params.sourceFile || 'resources/assets/index.js';
+			Config.outputName = params.outputName || process.env.npm_package_name || 'app';
+			Config.extractVueStyles = '[name].css';
+			Config.postCss.push(
+				require('postcss-url')({
+					url: (asset, dir, options, decl, warn, result) => {
+						if (path.isAbsolute(asset.originUrl)) {
+							const relativeUrl = path.relative(decl.source.input.file, asset.absolutePath);
+							return relativeUrl.replace(/\\/g, '////');
+						}
+					}
+				})
+			);
 
-    register(params) {
-        Config.sourceFile = params.sourceFile || 'resources/assets/index.js';
-        Config.outputName = params.outputName || process.env.npm_package_name || 'app';
-        Config.extractVueStyles = '[name].css';
-        Config.postCss.push(
-            require('postcss-url')({
-                url: (asset, dir, options, decl, warn, result) => {
-                    if (path.isAbsolute(asset.originUrl)) {
-                        const relativeUrl = path.relative(decl.source.input.file, asset.absolutePath);
-                        return relativeUrl.replace(/\\/g, '////');
-                    }
-                }
-            })
-        );
+			mix
+				.js(Config.sourceFile, '.build/package.js')
+				.extract()
+				.copy(Config.publicPath + '/.build/*.js', Config.publicPath + '/js/')
+				.copy(Config.publicPath + '/.build/*.css', Config.publicPath + '/css/');
+		}
 
-        mix
-            .js(Config.sourceFile, '.build/package.js')
-            .extract()
-            .babel(Config.publicPath + '/.build/**/!(manifest).js', Config.publicPath + '/js/' + Config.outputName + (Mix.inProduction() ? '.min.js' : '.js'))
-            .copy(Config.publicPath + '/.build/manifest.js', Config.publicPath + '/js/')
-            .babel(Config.publicPath + '/.build/*.css', Config.publicPath + '/css/' + Config.outputName + (Mix.inProduction() ? '.min.css' : '.css'))
-    }
+		dependencies() {
+			const deps = pkg.sync().bundleDependencies || [];
+			deps.push('postcss-url', 'read-pkg-up', 'clean-webpack-plugin');
+			return deps;
+		}
 
-    dependencies() {
-        const deps = pkg.sync().pkg.bundleDependencies || [];
-        deps.push('postcss-url', 'read-pkg-up');
-        return deps;
-    }
-
-});
+		webpackConfig(webpackConfig) {
+			webpackConfig.plugins.push(
+				new CleanWebpackPlugin({
+					verbose: true,
+					cleanOnceBeforeBuildPatterns: [ './css', './js', './fonts' ],
+					cleanAfterEveryBuildPatterns: [ './.build' ]
+				})
+			);
+		}
+	}()
+);
